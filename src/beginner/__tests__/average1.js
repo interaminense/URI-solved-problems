7e2f4e77c0bcb0a73d1b61a4a00bc1a815e13f51
Adriano <adriano.interaminense@gmail.com>
2018-07-29 22:16:23 -0300

    Solving problem average1 with jest

---
+++ src/beginner/__tests__/average1.js
@@ -0,0 +1,27 @@
+import {average, tmpl} from '../average1';
+
+describe('Average 1', () => {
+	let A = 0;
+	let B = 0;
+
+	it('The average for 5.0 and 7.1 must be 6.43182', () => {
+		A = 5.0;
+		B = 7.1;
+
+		expect(average(A, B)).toEqual(tmpl((6.43182).toFixed(5)));
+	});
+
+	it('The average for 0.0 and 7.1 must be 4.84091', () => {
+		A = 0.0;
+		B = 7.1;
+
+		expect(average(A, B)).toEqual(tmpl((4.84091).toFixed(5)));
+	});
+
+	it('The average 0 + 0 must be 10.00000', () => {
+		A = 10.0;
+		B = 10.0;
+
+		expect(average(A, B)).toEqual(tmpl((10.00000).toFixed(5)));
+	});
+});
