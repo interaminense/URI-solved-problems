2fd6f76a1137edb7fedc35d8e190e1d4500a96e8
Adriano <adriano.interaminense@gmail.com>
2018-07-29 22:15:58 -0300

    Solving problem simple-product with jest

---
+++ src/beginner/__tests__/simple-product.js
@@ -0,0 +1,27 @@
+import {prod, tmpl} from '../simple-product';
+
+describe('Simple prod', () => {
+	let A = 0;
+	let B = 0;
+
+	it('The prod 3 * 9 must be 27', () => {
+		A = 3;
+		B = 9;
+
+		expect(prod(A, B)).toEqual(tmpl(27));
+	});
+
+	it('The prod -30 * 10 must be -300', () => {
+		A = -30;
+		B = 10;
+
+		expect(prod(A, B)).toEqual(tmpl(-300));
+	});
+
+	it('The prod 0 * 9 must be 0', () => {
+		A = 0;
+		B = 9;
+
+		expect(prod(A, B)).toEqual(tmpl(0));
+	});
+});
