bc249a53e1f8bf5280d34716eb88ff211cdfd69e
Adriano <adriano.interaminense@gmail.com>
2018-07-29 22:15:20 -0300

    Update test to area-of-circle

--- src/beginner/__tests__/area-of-a-circle.js
+++ src/beginner/__tests__/area-of-a-circle.js
@@ -6,18 +6,18 @@ describe('Area of a circle', () => {
 	it('The raius is 2.00, so, the area of circle should be 12.5664', () => {
 		R = 2.00;
 
-		expect(areaOfCircle(R)).toEqual(tmpl('12.5664'));
+		expect(areaOfCircle(R)).toEqual(tmpl((12.5664).toFixed(4)));
 	});
 
 	it('The raius is 100.64, so, the area of circle should be 31819.3103', () => {
 		R = 100.64;
 
-		expect(areaOfCircle(R)).toEqual(tmpl('31819.3103'));
+		expect(areaOfCircle(R)).toEqual(tmpl((31819.3103).toFixed(4)));
 	});
 
 	it('The raius is 150.00, so, the area of circle should be 70685.7750', () => {
 		R = 150.00;
 
-		expect(areaOfCircle(R)).toEqual(tmpl('70685.7750'));
+		expect(areaOfCircle(R)).toEqual(tmpl((70685.7750).toFixed(4)));
 	});
 });
