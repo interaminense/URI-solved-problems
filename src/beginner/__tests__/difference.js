3c0b68051a801672c6f9e140fede7f92d22bddee
Adriano <adriano.interaminense@gmail.com>
2018-08-05 23:19:21 -0300

    Add difference solved problem

---
+++ src/beginner/__tests__/difference.js
@@ -0,0 +1,35 @@
+import { difference, tmpl } from '../difference';
+
+describe('Difference', () => {
+  let a = 0;
+  let b = 0;
+  let c = 0;
+  let d = 0;
+
+  it('The differente between 5, 6, 7 and 8 is -26', () => {
+    a = 5;
+    b = 6;
+    c = 7;
+    d = 8;
+
+    expect(difference(a, b, c, d)).toEqual(tmpl((-26)));
+  });
+
+  it('The differente between 0, 0, 7 and 8 is -56', () => {
+    a = 0;
+    b = 0;
+    c = 7;
+    d = 8;
+
+    expect(difference(a, b, c, d)).toEqual(tmpl((-56)));
+  });
+
+  it('The differente between 5, 6, -7 and 8 is 86', () => {
+    a = 5;
+    b = 6;
+    c = -7;
+    d = 8;
+
+    expect(difference(a, b, c, d)).toEqual(tmpl((86)));
+  });
+});
