678ae72be4d7fb118e843e3c2cd40cb7a1a1f45e
Adriano <adriano.interaminense@gmail.com>
2018-08-08 00:29:31 -0300

    Add salary-with-bonus solved problem

---
+++ src/beginner/__tests__/salary-with-bonus.js
@@ -0,0 +1,32 @@
+import { salary, tmpl } from '../salary-with-bonus';
+
+describe('Salary with bonus', () => {
+  let name = '';
+  let fixedSalary = 0;
+  let salesTotal = 0;
+
+  it('The fixed salary of JOAO is 500 and sallers total is 1230.30, his bonus is 684.54', () => {
+    name = 'JOAO';
+    fixedSalary = 500;
+    salesTotal = 1230.30;
+
+    expect(salary(name, fixedSalary, salesTotal)).toEqual(tmpl(684.54));
+    expect(salary(name, fixedSalary, salesTotal)).toMatchSnapshot();
+  });
+
+  it('The fixed salary of PEDRO is 700.00 and sallers total is 0, his bonus is 700', () => {
+    name = 'PEDRO';
+    fixedSalary = 700;
+    salesTotal = 0;
+
+    expect(salary(name, fixedSalary, salesTotal)).toEqual(tmpl('700.00'));
+  });
+
+  it('The fixed salary of MANGOJATA is 1700 and sallers total is 1230.50, his bonus is 1884.58', () => {
+    name = 'MANGOJATA';
+    fixedSalary = 1700;
+    salesTotal = 1230.50;
+
+    expect(salary(name, fixedSalary, salesTotal)).toEqual(tmpl(1884.58));
+  });
+});
---
+++ src/beginner/salary-with-bonus.js
@@ -0,0 +1,25 @@
+/**
+ * Make a program that reads a seller's name, his/her fixed salary and the sale's total made by
+ * himself/herself in the month (in money). Considering that this seller receives 15% over all
+ * products sold, write the final salary (total) of this seller at the end of the month , with two
+ * decimal places.
+ *
+ * INPUT
+ *
+ * The input file contains a text (employee's first name), and two double precision values, that
+ * are the seller's salary and the total value sold by him/her.
+ *
+ * OUTPUT
+ *
+ * Print the seller's total salary, according to the given example
+ */
+
+const tmpl = salary => `TOTAL = ${salary}`;
+
+const salary = (name, fixedSalary, salesTotal) => {
+  const totalSalary = (salesTotal * 0.15) + fixedSalary;
+
+  return tmpl(totalSalary.toFixed(2));
+};
+
+export { tmpl, salary };
